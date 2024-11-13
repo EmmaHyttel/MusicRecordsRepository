f0a4cc0e4334d3fc1be002b36a71642b20e8184a
NannaRister <nannarister@hotmail.com>
2024-11-13 10:39:14 +0100

    Validation, Hascode, Equals + testklasse men ikke implementeret

--- MusicRecordsRepository/Models/MusicRecords.cs
+++ MusicRecordsRepository/Models/MusicRecords.cs
@@ -2,14 +2,16 @@
 
 public class MusicRecords
 {
+    public int Id { get; set; }
     public string Title { get; set; }
     public string Artist { get; set; }
     public int Duration { get; set; }
     public int PublicationYear { get; set; }
     public string Genre { get; set; }
 
-    public MusicRecords(string title, string artist, int duration, int publicationYear, string genre)
+    public MusicRecords(int id, string title, string artist, int duration, int publicationYear, string genre)
     {
+        Id = id;
         Title = title;
         Artist = artist;
         Duration = duration;
@@ -21,8 +23,89 @@ public class MusicRecords
     {
     }
 
+    public void ValidateTitle()
+    {
+        if (Title == null)
+        {
+            throw new ArgumentNullException("Title is null");
+        }
+
+        if (Title.Length < 1)
+        {
+            throw new ArgumentException("Title must be at least 1 character: " + Title);
+        }
+    }
+
+    public void ValidateArtist()
+    {
+        if (Artist == null)
+        {
+            throw new ArgumentNullException("Artist is null");
+        }
+
+        if (Title.Length < 1)
+        {
+            throw new ArgumentException("Artist must be at least 1 character: " + Artist);
+        }
+    }
+
+    public void ValidateDuration()
+    {
+        if (Duration < 30 || Duration > 4800)
+        {
+            throw new ArgumentOutOfRangeException("Duration must be minimum 30 seconds and maximum 4800 seconds: " + Duration);
+        }
+    }
+
+    public void ValidatePublicationYear()
+    {
+        if (PublicationYear < 1982)
+            throw new ArgumentOutOfRangeException("Publication year must be 1982 or after: " + PublicationYear);
+    }
+
+    public void ValidateGenre()
+    {
+        if (Genre == null)
+        {
+            throw new ArgumentNullException("Genre is null");
+        }
+
+        if (Genre.Length < 3)
+        {
+            throw new ArgumentException("Genre must be at least 3 character: " + Genre);
+        }
+    }
+
+    public void Validate()
+    {
+        ValidateTitle();
+        ValidateArtist();
+        ValidateDuration();
+        ValidatePublicationYear();
+        ValidateGenre();
+    }
+
+    // Hvad er Equals() --> hvordan implementeres det og hvorfor --> override 
+    // Implementer også GetHashCode() --> Hvad er det og hvorfor implementeres det --> override 
+    // fx 
+    public override bool Equals(object? obj)
+    {
+        return obj is MusicRecords record &&
+            Id == record.Id &&
+            Title == record.Title &&
+            Artist == record.Artist &&
+            Duration == record.Duration &&
+            PublicationYear == record.PublicationYear &&
+            Genre == record.Genre;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Title, Artist, Duration, PublicationYear, Genre);
+    }
+
     public override string ToString()
     {
-        return $"{{{nameof(Title)}={Title}, {nameof(Artist)}={Artist}, {nameof(Duration)}={Duration.ToString()}, {nameof(PublicationYear)}={PublicationYear.ToString()}, {nameof(Genre)}={Genre}}}";
+        return $"{Id}, {Title}, {Artist}, {Duration}, {PublicationYear}, {Genre}";
     }
 }
---
+++ MusicRecordsUnitTest/MusicRecordsTest.cs
@@ -0,0 +1,11 @@
+namespace MusicRecordsUnitTest
+{
+    [TestClass]
+    public class MusicRecordsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
